8a9b89fe7a0366df35bcac093db3764b308ce46b
Felipe F. Lima <felipe@ferreiradelima.com>
2014-02-17 22:32:48 -0300

    Adicionado validator

--- services/clients.js
+++ services/clients.js
@@ -1,12 +1,19 @@
+var validator = require('validator');
+
 function getall(req, res) {
     var all = [
         {
             "name": "Felipe"
         },
         {
-            "name": "Gellati"
+            "name": "Gellati",
+            "mail": "bozo@cor"
         }
     ];
+
+    if (validator.isEmail(all[1].mail)) {
+        console.log('email valido');
+    }
 
     DB.collection('nomes').insert(all, function (err) {
         if (err) {
