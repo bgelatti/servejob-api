b76f62c9564e4287008fdf33a40085540e1ee3b2
unknown <bgelatti@gmail.com>
2014-02-19 20:44:24 -0300

    Corrigido identação

--- services/jobs.js
+++ services/jobs.js
@@ -1,28 +1,28 @@
 var validator = require('validator');
 
 function saveJob(req, res){
-	var job = {
-		"compName" : req.query.compName,
-        "compMail" : req.query.compMail,
-        "compWeb" : req.query.compWeb,
-        "expireDate" : req.query.expireDate,
-        "deletePassword" : req.query.deletePassword,
-        "jobTittle" : req.query.jobTittle,
-        "jobType" : req.query.jobType, 
-        "remote" : req.query.remote,
-        "jobLocation" : req.query.jobLocation,
-        "jobDesc" : req.query.jobDesc
-	}
+    var job = {
+        "compName": req.query.compName,
+        "compMail": req.query.compMail,
+        "compWeb": req.query.compWeb,
+        "expireDate": req.query.expireDate,
+        "deletePassword": req.query.deletePassword,
+        "jobTittle": req.query.jobTittle,
+        "jobType": req.query.jobType,
+        "remote": req.query.remote,
+        "jobLocation": req.query.jobLocation,
+        "jobDesc": req.query.jobDesc
+    }
 
-	if (validator.isEmail(job.compMail)) {
+    if (validator.isEmail(job.compMail)) {
         console.log('email valido');
     }
 
     if (validator.isDate(job.expireDate)){
-    	console.log('data valida');
+        console.log('data valida');
     }
 
-	DB.collection('jobs').insert(job, function (err) {
+    DB.collection('jobs').insert(job, function (err) {
         if (err) {
             res.send(err);
         } else {
@@ -32,7 +32,7 @@ function saveJob(req, res){
 };
 
 function getAllJobs(req, res){
-	var collection = DB.collection('jobs').find({}).toArray(function(err, docs) {
+    var collection = DB.collection('jobs').find({}).toArray(function(err, docs) {
         console.dir(docs);
     });
 }
