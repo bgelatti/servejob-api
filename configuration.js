4b3d6a8326dfcdc9b18d0a4bafc5b6ce2e5d9409
Felipe F. Lima <felipe@ferreiradelima.com>
2014-03-07 00:02:46 -0300

    Modificado nomes de variaveis de sistema

--- configuration.js
+++ configuration.js
@@ -1,17 +1,17 @@
 var format  = require('util').format;
 var env     = process.env;
 var configs = {
-    "port"                 : env.PORT_BACKEND      || 7050,
-    "connectionString"     : env.ACCOUNTS_DB       || 'mongodb://localhost/servejob',
-    "connectionString_test": env.ACCOUNTS_TEST     || 'mongodb://localhost/servejob-test'
+    "port"                  : env.PORT    || 7050,
+    "connectionString"      : env.DB      || 'mongodb://localhost/servejob',
+    "connectionString_test" : env.DB_TEST || 'mongodb://localhost/servejob-test'
 };
 
-if (process.env.WERCKER_MONGODB_HOST) {
-    var model, connectionString;
-
-    model = 'mongodb://%s:%s/servejob-test';
-    connectionString = format(model, env.WERCKER_MONGODB_HOST, env.WERCKER_MONGODB_PORT);
-    configs.connectionString_test = connectionString;
-}
+// if (process.env.WERCKER_MONGODB_HOST) {
+//     var model, connectionString;
+//
+//     model = 'mongodb://%s:%s/servejob-test';
+//     connectionString = format(model, env.WERCKER_MONGODB_HOST, env.WERCKER_MONGODB_PORT);
+//     configs.connectionString_test = connectionString;
+// }
 
 module.exports = configs;
