f7666487cfd4b5ed5b1450de2f401fc3ae6276b5
Ubuntu <bgelatti@gmail.com>
2014-04-19 20:37:52 -0300

    Adicionado sitemap

--- services/sitemap.js
+++ services/sitemap.js
@@ -1,34 +1,36 @@
 /*jslint node:true, unparam:true, nomen:true*/
+/*globals DB*/
+'use strict';
 var sm = require('sitemap');
 var staticsitemap = require('../symbols/sitemap.json');
 
 function get(req, res) {
-    var filter;
+    var filter, sitelist, obj, sitemap;
 
     filter = {
         '_id': 0,
         'permalink': 1
     };
 
-    DB.collection('jobs').find({}, filter).toArray(function (err, data){
-        var sitelist = [];
-        for (var i in data) {
-            var obj = {
-                'url': '/job/' + data[i].permalink + '/',
+    DB.collection('jobs').find({}, filter).toArray(function (err, data) {
+        sitelist = [];
+        data.forEach(function (i) {
+            obj = {
+                'url': '/job/' + i.permalink + '/',
                 'changefreq': 'daily',
                 'priority': 0.3
-            }
+            };
             sitelist.push(obj);
-        }
+        });
         sitelist = sitelist.concat(staticsitemap);
         sitemap = sm.createSitemap({
             'hostname': 'http://servejob.com',
             'cacheTime': 600000,
             'urls': sitelist
         });
-        sitemap.toXML( function (xml) {
+        sitemap.toXML(function (xml) {
             res.header('Content-Type', 'application/xml');
-            res.send( xml );
+            res.send(xml);
         });
     });
 }
